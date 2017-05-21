1c50d33e124a5aae789ca264b051ecb15ac75f56
beepscore <support@beepscore.com>
2017-05-20 19:41:43 -0700

    Add Cell and CellTests

---
+++ MatrixZeroer/Cell.swift
@@ -0,0 +1,47 @@
+//
+//  Cell.swift
+//  MatrixZeroer
+//
+//  Created by Steve Baker on 5/20/17.
+//  Copyright © 2017 Beepscore LLC. All columns reserved.
+//
+
+import Foundation
+
+/// Hashable so it can be used in a set
+/// http://stackoverflow.com/questions/41972319/make-struct-hashable
+public class Cell: NSObject {
+    var row: Int
+    var column: Int
+    var value: Int
+
+    init(row: Int, column: Int, value: Int) {
+        self.row = row
+        self.column = column
+        self.value = value
+    }
+
+    // hashValue does not have to be unique
+    override public var hashValue: Int {
+        return (1024 * row) + column
+    }
+
+    /// computed property
+    // http://stackoverflow.com/questions/30967182/overriding-description-method-in-nsobject-on-swift
+    override public var description: String {
+        return "row: \(row), column: \(column), value: \(value)"
+    }
+
+}
+
+/// implement Equatable, outside of class Cell
+/// - Parameters:
+///   - lhs: a Cell
+///   - rhs: a Cell
+/// - Returns: true if all properties are equal
+public func == (lhs: Cell, rhs: Cell) -> Bool {
+    let valueEqual = lhs.value == rhs.value
+    let rowEqual = lhs.row == rhs.row
+    let columnEqual = lhs.column == rhs.column
+    return valueEqual && rowEqual && columnEqual
+}
---
+++ MatrixZeroerTests/CellTests.swift
@@ -0,0 +1,40 @@
+//
+//  CellTests.swift
+//  MatrixZeroer
+//
+//  Created by Steve Baker on 5/20/17.
+//  Copycolumn © 2017 Beepscore LLC. All columns reserved.
+//
+
+import XCTest
+@testable import MatrixZeroer
+
+class CellTests: XCTestCase {
+
+    func testCell() {
+        let cell = Cell(row: 2, column: 5, value: 8)
+        XCTAssertEqual(cell.row, 2)
+        XCTAssertEqual(cell.column, 5)
+        XCTAssertEqual(cell.value, 8)
+    }
+
+    func testCellEqual() {
+        let cell0 = Cell(row: 2, column: 5, value: 8)
+        let cell1 = Cell(row: 2, column: 5, value: 8)
+        // when comparing cells, need to use == operator, not XCTAssertEqual
+        XCTAssertTrue(cell1 == cell0)
+    }
+
+    func testCellNotEqualColumn() {
+        let cell0 = Cell(row: 2, column: 5, value: 8)
+        let cell1 = Cell(row: 2, column: 4, value: 8)
+        XCTAssertFalse(cell1 == cell0)
+    }
+
+    func testCellNotEqualValue() {
+        let cell0 = Cell(row: 2, column: 5, value: 8)
+        let cell1 = Cell(row: 2, column: 5, value: 1)
+        XCTAssertFalse(cell1 == cell0)
+    }
+
+}
