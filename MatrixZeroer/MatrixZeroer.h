42e276f79bce154763c7bf45117fa96abd7ba6bb
beepscore <support@beepscore.com>
2017-05-21 23:35:07 -0700

    Convert project from single view app to framework for simplicity.
    Procedure-
    In Xcode delete targets for app and tests and all related files except xcodeproj. Add targets for framework and tests, reusing name MatrixZeroer.
    Copy source code files like Cell.swift and MatrixZeroer.swift from app into framework.

---
+++ MatrixZeroer/MatrixZeroer.h
@@ -0,0 +1,19 @@
+//
+//  MatrixZeroer.h
+//  MatrixZeroer
+//
+//  Created by Steve Baker on 5/21/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MatrixZeroer.
+FOUNDATION_EXPORT double MatrixZeroerVersionNumber;
+
+//! Project version string for MatrixZeroer.
+FOUNDATION_EXPORT const unsigned char MatrixZeroerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MatrixZeroer/PublicHeader.h>
+
+
